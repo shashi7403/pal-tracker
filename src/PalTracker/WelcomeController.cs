3f63371fed80a1c22f8931ba8e29124c9db1c5b5
unknown <shashi.kumar066@gmail.com>
2020-06-24 05:07:16 +0000

    version 5.2

--- src/PalTracker/WelcomeController.cs
+++ src/PalTracker/WelcomeController.cs
@@ -9,7 +9,7 @@ namespace PalTracker
 
         [HttpGet]
         public string SayHello() => _message.Message;
-
+//
         public WelcomeController(WelcomeMessage message)
         {
             _message = message;
